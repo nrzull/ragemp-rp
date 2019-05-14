e9e9e954a9b9a76fc3a3eee41829efa8b980f387
nrzull <nrzull@tuta.io>
2019-05-14 22:52:44 +0500

    change batch scripts to bash scripts

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
-@echo off
 mkdir temp
 cp -r mp/bridge/runtime temp/_runtime
 cd temp && curl -LO https://cdn.gtanet.work/bridge-package.zip && cd ..
-cd temp && tar -xvf bridge-package.zip && cd ..
+cd temp && unzip bridge-package.zip && cd ..
 cp -r temp/bridge/runtime/* mp/bridge/runtime
 cp -r temp/_runtime/* mp/bridge/runtime
 cp -r temp/bridge/vehicleData.json mp/bridge/vehicleData.json
