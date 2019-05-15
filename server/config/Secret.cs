703fa0537413ae78c61d9bbe9a9344f4f32914b3
nrzull <nrzull@tuta.io>
2019-05-15 16:49:37 +0500

    remove Secret.cs from git

--- server/config/Secret.cs
+++
@@ -1,9 +0,0 @@
-namespace Aquamarine.Server.Config
-{
-    class Secret
-    {
-        public const string Database = "Aquamarine-Rp";
-        public const string Username = "postgres";
-        public const string Password = "qaz963456";
-    }
-}
