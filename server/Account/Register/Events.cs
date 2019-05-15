dc98371acf1d8ab751c00036af5007696b93924d
1cover <coverboy@yandex.ru>
2019-05-15 17:15:14 +0300

    Initial Register

---
+++ server/Account/Register/Events.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GTANetworkAPI;
+
+namespace Aquamarine.Server.Account.Register
+{
+    class Events : Script
+    {
+    }
+}
---
+++ server/Account/Register/Service.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aquamarine.Server.Account.Register
+{
+    class Service
+    {
+    }
+}
