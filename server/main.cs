038ebc9bfe245deab9f36e77e43415f2879890a4
1cover <coverboy@yandex.ru>
2019-05-15 13:47:26 +0300

    Reset Main.cs

--- server/main.cs
+++ server/main.cs
@@ -2,7 +2,6 @@ using GTANetworkAPI;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.EntityFrameworkCore;
 
 namespace Aquamarine.Server
 {
@@ -11,25 +10,6 @@ namespace Aquamarine.Server
         public Main()
         {
 
-        }
-
-        [ServerEvent(Event.PlayerSpawn)]
-        public void OnPlayerSpawn(Client player)
-        {
-            try
-            {
-                using (Database connection = new Database())
-                {
-                    connection.Accounts.Add(new Account { Name = player.Name + " TEST" });
-                    connection.SaveChanges();
-
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
         }
     }
 }
