5ef6c86ddbe9bba062b47f645605d538cf55a3d0
1cover <coverboy@yandex.ru>
2019-05-15 17:14:45 +0300

    Initial Login

---
+++ server/Account/Login/Events.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GTANetworkAPI;
+
+namespace Aquamarine.Server.Account.Login
+{
+    class Events : Script
+    {
+    }
+}
---
+++ server/Account/Login/Service.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aquamarine.Server.Account.Login
+{
+    class Service
+    {
+    }
+}
