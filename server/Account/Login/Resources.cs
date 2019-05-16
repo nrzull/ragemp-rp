33a3136916b2d3844d45b10b000099af09143405
nrzull <nrzull@tuta.io>
2019-05-16 09:37:58 +0500

    init login feature

---
+++ server/Account/Login/Resources.cs
@@ -0,0 +1,7 @@
+namespace Aquamarine.Server.Account.Login
+{
+    public static class Resources
+    {
+        public const string ERROR_INCORRECT_LOGIN_OR_PASSWORD = "Неправильный логин или пароль";
+    }
+}
--- server/Account/Login/Service.cs
+++ server/Account/Login/Service.cs
@@ -1,10 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using GTANetworkAPI;
 
 namespace Aquamarine.Server.Account.Login
 {
     class Service
     {
+        public static void LogIn(Client player, string login, string password)
+        {
+            Dictionary<string, string> result = ValidateFields(login, password);
+
+            if (result != null)
+            {
+                var errors = string.Join("\n", result.ToArray());
+                player.TriggerEvent(Shared.Events.LOGIN_ERROR, errors);
+                return;
+            }
+
+            Account.Entity account = Account.Service.GetAccountEntityByLogin(login);
+            if (account == null)
+            {
+                player.TriggerEvent(Shared.Events.LOGIN_ERROR, Resources.ERROR_INCORRECT_LOGIN_OR_PASSWORD);
+                return;
+            }
+
+            if (VerifyPassword(account, password) == false)
+            {
+                player.TriggerEvent(Shared.Events.LOGIN_ERROR, Resources.ERROR_INCORRECT_LOGIN_OR_PASSWORD);
+                return;
+            }
+
+            // TODO continue logic
+        }
+
+        static Dictionary<string, string> ValidateFields(string login, string password)
+        {
+            var errors = new Dictionary<string, string>();
+
+            var error = Account.Service.ValidateLogin(login);
+            if (error is string)
+            {
+                errors.Add("login", error);
+            }
+
+            error = Account.Service.ValidatePassword(password);
+            if (error is string)
+            {
+                errors.Add("password", error);
+            }
+
+            if (errors.Count > 0) return errors;
+            return null;
+        }
+
+        // TODO use Bcrypt for verifying
+        static bool VerifyPassword(Account.Entity account, string input)
+        {
+            return account.Password == input;
+        }
     }
 }
