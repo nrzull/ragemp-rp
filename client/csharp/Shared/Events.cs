3a479dd1709b659e354e9415adb5fc863747e600
1cover <coverboy@yandex.ru>
2019-05-17 23:22:18 +0300

    Update Events.cs

--- client/csharp/Shared/Events.cs
+++ client/csharp/Shared/Events.cs
@@ -2,15 +2,17 @@ namespace Project.Shared
 {
     class Events
     {
-        public const string REGISTER_ACCOUNT = "RegisterAccount";
-        public const string LOGIN_ACCOUNT = "LoginPlayer";
-        public const string REGISTER_ERROR = "RegisterError";
-        public const string LOGIN_ERROR = "LoginError";
-
-        public const string PLAYER_CONNECT = "PlayerConnect";
+        public const string PLAYER_CONNECT = "PLAYER_CONNECT";
 
         // TODO maybe use the same name of variable for value?
         public const string UI_LOADED = "UI_LOADED";
+
         public const string UI_LOGIN_SHOW = "UI_LOGIN_SHOW";
+        public const string UI_LOGIN_SUBMIT = "UI_LOGIN_SUBMIT";
+        public const string UI_LOGIN_ERROR = "UI_LOGIN_ERROR";
+
+        public const string UI_REGISTER_SHOW = "UI_REGISTER_SHOW";
+        public const string UI_REGISTER_SUBMIT = "UI_REGISTER_SUBMIT";
+        public const string UI_REGISTER_ERROR = "UI_REGISTER_ERROR";
     }
 }
