2a685d8ba7a5f4b9d7dbee0978585c43768690fd
1cover <coverboy@yandex.ru>
2019-05-17 23:15:15 +0300

    Update login view.jsx

--- ui/src/modules/login/view.jsx
+++ ui/src/modules/login/view.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 import "./styles.scss";
 
-function Login() {
-  return <div className="login">Login Component</div>;
+function View(props) {
+  return (
+    <div className="login">
+      <div>
+        <input
+          className="login__field"
+          type="text"
+          value={props.username}
+          onChange={props.onChangeUsername}
+          placeholder="Username"
+        />
+      </div>
+      <p className="login__error">{props.errors.username}</p>
+      <div>
+        <input
+          className="login__field"
+          type="text"
+          value={props.password}
+          onChange={props.onChangePassword}
+          placeholder="Password"
+        />
+      </div>
+      <p className="login__error">{props.errors.password}</p>
+      <div>
+        <label>
+          <input
+            className="login__checkbox"
+            type="checkbox"
+            defaultChecked={props.remember}
+            onChange={props.onChangeRemember}
+          />
+          Запомнить меня
+        </label>
+      </div>
+      <div>
+        <button className="login__submit" onClick={props.onClickSubmit}>
+          Войти
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export { Login };
+export { View };
