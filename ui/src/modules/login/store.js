8413dc814921e2f291b3e14652f3c743f2474852
1cover <coverboy@yandex.ru>
2019-05-17 23:13:51 +0300

    Update login store.js

--- ui/src/modules/login/store.js
+++ ui/src/modules/login/store.js
@@ -1,21 +1,64 @@
 const initialState = {
-  show: false
+  show: false,
+  username: "",
+  password: "",
+  remember: false,
+  loading: false,
+  errors: {
+    username: "",
+    password: ""
+  }
 };
 
-const types = new (class {
-  SHOW = "LOGIN_SHOW";
-})();
+const types = {
+  SHOW: "LOGIN_SHOW",
+  SET_USERNAME: "LOGIN_SET_USERNAME",
+  SET_PASSWORD: "LOGIN_SET_PASSWORD",
+  SET_REMEMBER: "LOGIN_SET_REMEMBER",
+  SET_LOADING: "LOGIN_SET_LOADING",
+  SET_ERRORS: "LOGIN_SET_ERRORS"
+};
 
-const actions = new (class {
+const actions = {
   show(payload) {
     return { type: types.SHOW, payload };
+  },
+
+  setUsername(payload) {
+    return { type: types.SET_USERNAME, payload };
+  },
+
+  setPassword(payload) {
+    return { type: types.SET_PASSWORD, payload };
+  },
+
+  setRemember(payload) {
+    return { type: types.SET_REMEMBER, payload };
+  },
+
+  setLoading(payload) {
+    return { type: types.SET_LOADING, payload };
+  },
+
+  setErrors(payload) {
+    return { type: types.SET_ERRORS, payload };
   }
-})();
+};
 
 function reducer(state = initialState, { type, payload }) {
   switch (type) {
     case types.SHOW:
       return { ...state, show: payload };
+    case types.SET_USERNAME:
+      return { ...state, username: payload };
+    case types.SET_PASSWORD:
+      return { ...state, password: payload };
+    case types.SET_REMEMBER:
+      return { ...state, remember: payload };
+    case types.SET_LOADING:
+      return { ...state, loading: payload };
+    case types.SET_ERRORS:
+      return { ...state, errors: payload };
 
     default:
       return state;
