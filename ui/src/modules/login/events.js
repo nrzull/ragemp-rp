041f2d269fdd80381ee4da41a26db4d7029aa59e
1cover <coverboy@yandex.ru>
2019-05-17 23:14:29 +0300

    Define event UI_LOGIN_ERROR

--- ui/src/modules/login/events.js
+++ ui/src/modules/login/events.js
@@ -5,3 +5,7 @@ import { actions } from "@/modules/login/store";
 bus.on(shared.events.UI_LOGIN_SHOW, payload => {
   store.dispatch(actions.show(payload));
 });
+
+bus.on(shared.events.UI_LOGIN_ERROR, payload => {
+  store.dispatch(actions.setErrors(payload));
+});
