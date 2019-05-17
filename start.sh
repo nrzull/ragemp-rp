f60d4676d85b201f06d75f6dd075363a12fe7dc0
nrzull <nrzull@tuta.io>
2019-05-17 13:39:17 +0500

    update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 rm -rf mp/client_packages/*
 rm -rf mp/bridge/resources/*
-cp -r server/compiled/* mp/bridge/resources
 cp -r client/javascript/* mp/client_packages
 cp -r client/csharp mp/client_packages/cs_packages
 rm -rf mp/client_packages/cs_packages/*.csproj
 rm -rf mp/client_packages/cs_packages/bin
 rm -rf mp/client_packages/cs_packages/obj
-cp server/Config/conf.json mp/conf.json
-cp server/Config/settings.xml mp/bridge/settings.xml
-cp server/Config/meta.xml mp/bridge/resources/netcoreapp2.0/meta.xml
 cd ui && npx webpack && cd ..
 cd server && dotnet build && cd ..
+cp -r server/compiled/* mp/bridge/resources
+cp server/Config/meta.xml mp/bridge/resources/netcoreapp2.0/meta.xml
+cp server/Config/settings.xml mp/bridge/settings.xml
+cp server/Config/conf.json mp/conf.json
 cd mp && start server.exe && cd ..
