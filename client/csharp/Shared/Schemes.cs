b27f74f15a35ae5f8cea4c83e5a73af0400a88cc
1cover <coverboy@yandex.ru>
2019-05-22 16:34:09 +0300

    Initial Schemes.cs

---
+++ client/csharp/Shared/Schemes.cs
@@ -0,0 +1,12 @@
+namespace Project.Shared
+{
+    class Schemes
+    {
+        public class UiLoginSubmitPayload
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public bool Remember { get; set; }
+        }
+    }
+}
