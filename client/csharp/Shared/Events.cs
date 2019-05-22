7e3e9b3eb7e0ddb875d8267ca52aa81032d4f000
1cover <coverboy@yandex.ru>
2019-05-22 21:42:57 +0300

    Rename shared events

--- client/csharp/Shared/Events.cs
+++ client/csharp/Shared/Events.cs
@@ -4,13 +4,15 @@ namespace Project.Shared
     {
         public const string SERVER_DEBUG = "SERVER_DEBUG";
 
+        public const string SERVER_LOGIN_SUBMIT = "SERVER_LOGIN_SUBMIT";
+        public const string SERVER_REGISTER_SUBMIT = "SERVER_REGISTER_SUBMIT";
+
         public const string CLIENT_PLAYER_CONNECT = "CLIENT_PLAYER_CONNECT";
+        public const string CLIENT_LOBBY_SHOW = "CLIENT_LOBBY_SHOW";
+        public const string CLIENT_LOGIN_SUBMIT_OK = "CLIENT_LOGIN_SUBMIT_OK";
 
         public const string UI_LOADED = "UI_LOADED";
         public const string UI_AUTH_SHOW = "UI_AUTH_SHOW";
-
-        public const string SERVER_LOGIN_SUBMIT = "SERVER_LOGIN_SUBMIT";
-        public const string SERVER_REGISTER_SUBMIT = "SERVER_REGISTER_SUBMIT";
 
         public const string UI_LOGIN_SHOW = "UI_LOGIN_SHOW";
         public const string UI_LOGIN_SUBMIT_OK = "UI_LOGIN_SUBMIT_OK";
