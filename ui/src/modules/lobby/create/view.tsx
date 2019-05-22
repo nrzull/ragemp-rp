acadb431a78107e28db9727543b0ebe2599de74d
nrzull <nrzull@tuta.io>
2019-05-22 18:27:08 +0300

    add create character dna range inputs

--- ui/src/modules/lobby/create/view.tsx
+++ ui/src/modules/lobby/create/view.tsx
@@ -5,7 +5,7 @@ import DnaIcon from "@/assets/icons/dna.svg";
 import HairIcon from "@/assets/icons/hair.svg";
 import ClothesIcon from "@/assets/icons/clothes.svg";
 import IdCardIcon from "@/assets/icons/id-card.svg";
-import { Switcher, Button, Range, Input } from "@/components";
+import { Switcher, Button, Range, Input, Title } from "@/components";
 
 type TActiveGroup = "id-card" | "parents" | "dna" | "hair" | "clothes";
 
@@ -84,8 +84,120 @@ function View(props: TProps) {
           {props.activeGroup === "parents" && <>Parents</>}
           {props.activeGroup === "dna" && (
             <>
+              <Title>Нос</Title>
               <div className="lobby-create__input-block">
-                <div className="lobby-create__label">Высота бровей</div>
+                <div className="lobby-create__label">ширина носа</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">высота носа</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">длина носа</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">ширина переносицы</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">кончик носа</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">сдвиг переносицы</div>
+                <Range />
+              </div>
+
+              <Title>Брови</Title>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">высота бровей</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">ширина бровей</div>
+                <Range />
+              </div>
+
+              <Title>Скулы</Title>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">высота скул</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">ширина скул</div>
+                <Range />
+              </div>
+
+              <Title>Щеки</Title>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">ширина щёк</div>
+                <Range />
+              </div>
+
+              <Title>Глаза</Title>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">TODO глаза</div>
+                <Range />
+              </div>
+
+              <Title>Губы</Title>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">TODO губы</div>
+                <Range />
+              </div>
+
+              <Title>Челюсти</Title>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">ширина челюстей</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">высота челюстей</div>
+                <Range />
+              </div>
+
+              <Title>Подбородок</Title>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">длина подбородка</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">позиция подбородка</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">ширина подбородка</div>
+                <Range />
+              </div>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">TODO шэйп подбородка</div>
+                <Range />
+              </div>
+
+              <Title>Шея</Title>
+
+              <div className="lobby-create__input-block">
+                <div className="lobby-create__label">ширина шеи</div>
                 <Range />
               </div>
             </>
