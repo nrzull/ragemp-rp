0a7f790b81cc248e7ccaeeec3d826ac9eeee9314
1cover <coverboy@yandex.ru>
2019-05-22 21:43:48 +0300

    Add debug command 'info'

--- server/Debug.cs
+++ server/Debug.cs
@@ -6,13 +6,27 @@ using GTANetworkAPI;
 namespace Project.Server
 {
     class Debug : Script
-    { 
+    {
         [RemoteEvent(Shared.Events.SERVER_DEBUG)]
         public void OnDebug(Client player, string message)
         {
             if (Config.Secret.Core.DEBUG)
             {
                 Console.WriteLine(message);
+            }
+        }
+
+        [Command("info")]
+        public void OnSave(Client player)
+        {
+            if (Config.Secret.Core.DEBUG)
+            { 
+                player.SendChatMessage("Info recorded. Check console!");
+
+                Console.WriteLine($"Position: {player.Position}");
+                Console.WriteLine($"Heading: {player.Heading}");
+                Console.WriteLine($"Health: {player.Health}");
+                Console.WriteLine($"Armor: {player.Armor}");
             }
         }
     }
